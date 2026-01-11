a383e407d15f4a7d0f53900b593b3de065605cde
MrCraft18 <mariocaden@gmail.com>
2026-01-10 18:55:09 -0600

    feat: pass link and downloadDirectory from the command line

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 import MegabulkProcess from "./classes/MegabulkProcess/index.js"
 
-const main = new MegabulkProcess({ folderLink: link, downloadDirectory: path })
+const [,, link, downloadDirectory] = process.argv
+
+if (!link || !downloadDirectory) {
+    console.error("Usage: node index.js <mega-folder-link> <download-path>")
+    process.exit(1)
+}
+
+const main = new MegabulkProcess({ folderLink: link, downloadDirectory })
 
 await main.start()
